3b9171d52ce24262c5279e43017f2774d534e92a
gabyv <gabyv@GABS18>
2025-02-20 18:41:00 -0600

    Agregar archivos de proyecto.

---
+++ L01_NUMEROS_CARNET/Controllers/blogDBController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using L01_NUMEROS_CARNET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_NUMEROS_CARNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class blogDBController : ControllerBase
+    {
+        private readonly blogDBContext _blogDBContexto;
+        public blogDBController(blogDBContext blogDBContexto)
+        {
+            _blogDBContexto = blogDBContexto;
+        }
+    }
+}
---
+++ L01_NUMEROS_CARNET/Controllers/calificacionesController.cs
@@ -0,0 +1,33 @@
+﻿using L01_NUMEROS_CARNET.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_NUMEROS_CARNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class calificacionesController : ControllerBase
+    {
+        private readonly blogDBContext _blogDBContexto;
+
+        public calificacionesController(blogDBContext blogDBContexto)
+        {
+            _blogDBContexto = blogDBContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<calificaciones> Listadocalificaciones = _blogDBContexto.calificaciones.ToList();
+
+            if (Listadocalificaciones.Count == 0) 
+            {
+                return NotFound();
+            }
+
+            return Ok(Listadocalificaciones);
+        }
+    }
+}
+
